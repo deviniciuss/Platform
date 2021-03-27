1742c6469bbbab75b6defdccb1fd5b9e4dca54f3
Vinicius Rosa <vinicius_rosa98@outlook.com>
2021-03-27 14:59:16 +0000

    Project started informing the user's social class

---
+++ Platform/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+    class Person
+    {
+
+        public double _day { get; private set; }
+        public double _salary { get; private set; }
+
+        public Person (double day, double salary)
+        {
+            _day = day;
+            _salary = salary;
+        }
+
+        public double SalaryForday()
+        {
+            return _salary / _day;
+        }
+         
+        public string IncomePerson()
+        {
+            if (SalaryForday() < 2.0)
+            {
+               return  "Poor";
+            }
+            else if (SalaryForday() <= 10.0)
+            {
+                return "Low Income";
+            }
+            else if (SalaryForday() <= 20.0)
+            {
+                return "Middle Income";
+            }
+            else if (SalaryForday() <= 50.0)
+            {
+                return "Upper Middle-Income";
+            }
+            else if (SalaryForday() > 50.0)
+            {
+                return "Hight Income";
+            }
+            else
+            {
+                return "unknown";
+            }
+        }
+        
+ 
+
+    }
+}
---
+++ Platform/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Platform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("_______________________________");
+            Console.WriteLine();
+            Console.WriteLine("  *  WELCOME THE PLATFORM   *");
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("How many days for month you work?");
+            double day = double.Parse(Console.ReadLine());
+            Console.WriteLine("Which your monthly salary?");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Person p1 = new Person(day, salary);
+            Console.WriteLine();
+            Console.WriteLine("You are: " + p1.IncomePerson());
+            Console.WriteLine(" _______________________________________________");
+            Console.WriteLine("|   Global Population Distribuition by Income   |");
+            Console.WriteLine("|                                               |");
+            Console.WriteLine("|_______________________________________________|");
+            Console.WriteLine("| Poor                -         15%             | ");
+            Console.WriteLine("|_______________________________________________| ");
+            Console.WriteLine("| Low Income          -         56%             | ");
+            Console.WriteLine("|_______________________________________________| ");
+            Console.WriteLine("| Middle Income       -         13%             | ");
+            Console.WriteLine("|_______________________________________________| ");
+            Console.WriteLine("| Upper-middle Income -         9%              | ");
+            Console.WriteLine("|_______________________________________________| ");
+            Console.WriteLine("| Hight Income        -         7%              | ");
+            Console.WriteLine("|_______________________________________________| ");
+            Console.WriteLine();
+            Console.WriteLine("Reference: https://www.pewresearch.org/");
+
+
+        }
+    }
+}
